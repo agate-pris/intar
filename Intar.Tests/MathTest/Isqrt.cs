59c9b1510852e6d674fa0a0c1fdd4fcf1cd1a3d2
agate-pris <agate-pris@users.noreply.github.com>
2024-05-21 00:31:02 +0900

    Math.Isqrt 追加

---
+++ Intar.Tests/MathTest/Isqrt.cs
@@ -0,0 +1,58 @@
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+
+namespace AgatePris.Intar.Tests {
+    public partial class MathTest {
+        [Test]
+        public static void IsqrtTestUint() {
+            const uint max = 0xffff;
+            Assert.AreEqual(max, Math.Isqrt(uint.MaxValue));
+
+            var processorCount = Environment.ProcessorCount;
+            Parallel.For(0, processorCount, n => {
+                var begin = uint.MaxValue * (ulong)n / (ulong)processorCount;
+                var end = uint.MaxValue * (ulong)(n + 1) / (ulong)processorCount;
+                for (var x = (uint)begin; x < (uint)end; ++x) {
+                    var actual = Math.Isqrt(x);
+                    if (x < actual * actual) {
+                        Assert.Fail();
+                    }
+                    if (actual != max) {
+                        var incremented = actual + 1;
+                        if (x >= incremented * incremented) {
+                            Assert.Fail();
+                        }
+                    }
+                }
+            });
+        }
+
+        [Test]
+        public static void IsqrtTestUlong() {
+            const ulong max = 0xffff_ffff;
+            Assert.AreEqual(0, Math.Isqrt(0));
+            Assert.AreEqual(max, Math.Isqrt(ulong.MaxValue));
+
+            var processorCount = Environment.ProcessorCount;
+            Parallel.For(0, processorCount, n => {
+                var begin = 1 + max * (ulong)n / (ulong)processorCount;
+                var end = 1 + max * (ulong)(n + 1) / (ulong)processorCount;
+                for (var y = begin; y < end; ++y) {
+                    {
+                        var actual = Math.Isqrt(y * y);
+                        if (actual != y) {
+                            Assert.Fail($"y: {y}, actual: {actual}");
+                        }
+                    }
+                    {
+                        var actual = Math.Isqrt(y * y - 1);
+                        if (actual != y - 1) {
+                            Assert.Fail($"y: {y}, actual: {actual}");
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
