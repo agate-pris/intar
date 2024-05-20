9eddb81c4cee65985d54bae238eef93c2fb28168
agate-pris <agate-pris@users.noreply.github.com>
2024-05-21 00:30:59 +0900

    BitOperations.RotateLeft 追加

---
+++ Intar/BitOperations/Rotate.cs
@@ -0,0 +1,14 @@
+using System.Runtime.CompilerServices;
+
+namespace AgatePris.Intar {
+    public static partial class BitOperations {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static uint RotateLeft(uint x, int k) {
+            return (x << k) | (x >> (bitsOfInt - k));
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ulong RotateLeft(ulong x, int k) {
+            return (x << k) | (x >> (bitsOfLong - k));
+        }
+    }
+}
