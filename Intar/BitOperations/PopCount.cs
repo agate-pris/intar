9920f33609c3b80fb55e064625c38e52ac3fc3ed
agate-pris <agate-pris@users.noreply.github.com>
2024-05-21 00:31:02 +0900

    BitOperations.PopCount 追加

---
+++ Intar/BitOperations/PopCount.cs
@@ -0,0 +1,36 @@
+using System.Runtime.CompilerServices;
+
+namespace AgatePris.Intar {
+    public static partial class BitOperations {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int PopCount(uint x) {
+            const uint k55 = 0x5555_5555;
+            const uint k33 = 0x3333_3333;
+            const uint k0F = 0x0F0F_0F0F;
+            unchecked {
+                x -= x >> 1 & k55;
+                x = (x >> 2 & k33) + (x & k33);
+                x = (x >> 4) + x & k0F;
+                x += x >> 8;
+                x += x >> 16;
+                return (int)x & (bitsOfInt * 2 - 1);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int PopCount(ulong x) {
+            const ulong k55 = 0x5555_5555_5555_5555;
+            const ulong k33 = 0x3333_3333_3333_3333;
+            const ulong k0F = 0x0F0F_0F0F_0F0F_0F0F;
+            unchecked {
+                x -= x >> 1 & k55;
+                x = (x >> 2 & k33) + (x & k33);
+                x = (x >> 4) + x & k0F;
+                x += x >> 8;
+                x += x >> 16;
+                x += x >> 32;
+                return (int)x & (bitsOfLong * 2 - 1);
+            }
+        }
+    }
+}
