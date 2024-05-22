37f9c0f3cd5b3f2fc9a331df768c7ab7c4d5f511
agate-pris <agate-pris@users.noreply.github.com>
2024-05-22 21:52:46 +0900

    Sign メンバメソッドをリネーム

--- Intar.Tests/Integer/SignTest.cs
+++ Intar.Tests/Integer/SignTest.cs
@@ -5,35 +5,35 @@ namespace AgatePris.Intar.Tests.Integer {
     public class SignTest {
         [Test]
         public static void Test() {
-            Assert.IsTrue(0.IsNegativeOrZero());
-            Assert.IsTrue(0.IsPositiveOrZero());
+            Assert.IsTrue(0.IsNegativeInclusive());
+            Assert.IsTrue(0.IsPositiveInclusive());
             Assert.IsTrue(0.IsZero());
-            Assert.IsTrue(0L.IsNegativeOrZero());
-            Assert.IsTrue(0L.IsPositiveOrZero());
+            Assert.IsTrue(0L.IsNegativeInclusive());
+            Assert.IsTrue(0L.IsPositiveInclusive());
             Assert.IsTrue(0L.IsZero());
-            Assert.IsTrue(1.IsGreaterThanZero());
-            Assert.IsTrue(1.IsPositiveOrZero());
-            Assert.IsTrue(1L.IsGreaterThanZero());
-            Assert.IsTrue(1L.IsPositiveOrZero());
-            Assert.IsTrue((-1).IsLessThanZero());
-            Assert.IsTrue((-1).IsNegativeOrZero());
-            Assert.IsTrue((-1L).IsLessThanZero());
-            Assert.IsTrue((-1L).IsNegativeOrZero());
-            Assert.IsFalse(0.IsGreaterThanZero());
-            Assert.IsFalse(0.IsLessThanZero());
-            Assert.IsFalse(0L.IsGreaterThanZero());
-            Assert.IsFalse(0L.IsLessThanZero());
-            Assert.IsFalse(1.IsLessThanZero());
-            Assert.IsFalse(1.IsNegativeOrZero());
+            Assert.IsTrue(1.IsPositiveExclusive());
+            Assert.IsTrue(1.IsPositiveInclusive());
+            Assert.IsTrue(1L.IsPositiveExclusive());
+            Assert.IsTrue(1L.IsPositiveInclusive());
+            Assert.IsTrue((-1).IsNegativeExclusive());
+            Assert.IsTrue((-1).IsNegativeInclusive());
+            Assert.IsTrue((-1L).IsNegativeExclusive());
+            Assert.IsTrue((-1L).IsNegativeInclusive());
+            Assert.IsFalse(0.IsPositiveExclusive());
+            Assert.IsFalse(0.IsNegativeExclusive());
+            Assert.IsFalse(0L.IsPositiveExclusive());
+            Assert.IsFalse(0L.IsNegativeExclusive());
+            Assert.IsFalse(1.IsNegativeExclusive());
+            Assert.IsFalse(1.IsNegativeInclusive());
             Assert.IsFalse(1.IsZero());
-            Assert.IsFalse(1L.IsLessThanZero());
-            Assert.IsFalse(1L.IsNegativeOrZero());
+            Assert.IsFalse(1L.IsNegativeExclusive());
+            Assert.IsFalse(1L.IsNegativeInclusive());
             Assert.IsFalse(1L.IsZero());
-            Assert.IsFalse((-1).IsGreaterThanZero());
-            Assert.IsFalse((-1).IsPositiveOrZero());
+            Assert.IsFalse((-1).IsPositiveExclusive());
+            Assert.IsFalse((-1).IsPositiveInclusive());
             Assert.IsFalse((-1).IsZero());
-            Assert.IsFalse((-1L).IsGreaterThanZero());
-            Assert.IsFalse((-1L).IsPositiveOrZero());
+            Assert.IsFalse((-1L).IsPositiveExclusive());
+            Assert.IsFalse((-1L).IsPositiveInclusive());
             Assert.IsFalse((-1L).IsZero());
         }
     }
