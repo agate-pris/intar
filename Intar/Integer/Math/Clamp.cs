5aefde8f0610ec303371522ad722e2db373967c9
agate-pris <agate-pris@users.noreply.github.com>
2024-05-24 12:17:31 +0900

    Clamp.Min 追加

--- Intar/Integer/Math/Clamp.cs
+++ Intar/Integer/Math/Clamp.cs
@@ -9,6 +9,14 @@ using u64 = System.UInt64;
 
 namespace AgatePris.Intar.Integer {
     public static partial class Math {
+        public static i08 Min(this i08 x, i08 y) => x < y ? x : y;
+        public static i16 Min(this i16 x, i16 y) => x < y ? x : y;
+        public static i32 Min(this i32 x, i32 y) => x < y ? x : y;
+        public static i64 Min(this i64 x, i64 y) => x < y ? x : y;
+        public static u08 Min(this u08 x, u08 y) => x < y ? x : y;
+        public static u16 Min(this u16 x, u16 y) => x < y ? x : y;
+        public static u32 Min(this u32 x, u32 y) => x < y ? x : y;
+        public static u64 Min(this u64 x, u64 y) => x < y ? x : y;
         public static i08 Clamp(this i08 x, i08 min, i08 max) => x < min ? min : (x > max ? max : x);
         public static i16 Clamp(this i16 x, i16 min, i16 max) => x < min ? min : (x > max ? max : x);
         public static i32 Clamp(this i32 x, i32 min, i32 max) => x < min ? min : (x > max ? max : x);
