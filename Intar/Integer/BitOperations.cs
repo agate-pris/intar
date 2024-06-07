34223cafa8d6ff784a97ca9a798584a5d7d1a136
agate-pris <agate-pris@users.noreply.github.com>
2024-06-07 23:10:08 +0900

    .NET 7 の場合に未使用の変数がある問題を修正

--- Intar/Integer/BitOperations.cs
+++ Intar/Integer/BitOperations.cs
@@ -1,6 +1,8 @@
 namespace AgatePris.Intar.Integer {
     public static partial class BitOperations {
+#if !NET7_0_OR_GREATER
         const int bitsOfInt = sizeof(int) * 8;
         const int bitsOfLong = sizeof(long) * 8;
+#endif
     }
 }
