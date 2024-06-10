642bed76cee16946b504473ee9e8414e9bdd4678
agate-pris <agate-pris@users.noreply.github.com>
2024-06-11 00:28:38 +0900

    Sign リファクタリング & コメントを修正

--- Intar/Math.gen.cs
+++ Intar/Math.gen.cs
@@ -180,165 +180,71 @@ namespace AgatePris.Intar {
                 ? max : v;
         }
 
-#if NET7_0_OR_GREATER
-
         /// <summary>
-        /// この関数は .NET 7.0 以降の場合 <c>int.Sign</c> を呼ぶ.
-        /// .NET 6.0 以前の場合, 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
-        /// 符号なし整数型の場合 <c>v</c> が 0 の場合は 0 を, 0 より大きい場合は 1 を返す.
-        /// </summary>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int Sign(int v) => int.Sign(v);
-
-        /// <summary>
-        /// この関数は .NET 7.0 以降の場合 <c>uint.Sign</c> を呼ぶ.
-        /// .NET 6.0 以前の場合, 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
-        /// 符号なし整数型の場合 <c>v</c> が 0 の場合は 0 を, 0 より大きい場合は 1 を返す.
-        /// </summary>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int Sign(uint v) => uint.Sign(v);
-
-        /// <summary>
-        /// この関数は .NET 7.0 以降の場合 <c>long.Sign</c> を呼ぶ.
-        /// .NET 6.0 以前の場合, 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
-        /// 符号なし整数型の場合 <c>v</c> が 0 の場合は 0 を, 0 より大きい場合は 1 を返す.
-        /// </summary>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int Sign(long v) => long.Sign(v);
-
-        /// <summary>
-        /// この関数は .NET 7.0 以降の場合 <c>ulong.Sign</c> を呼ぶ.
-        /// .NET 6.0 以前の場合, 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
-        /// 符号なし整数型の場合 <c>v</c> が 0 の場合は 0 を, 0 より大きい場合は 1 を返す.
-        /// </summary>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int Sign(ulong v) => ulong.Sign(v);
-
-        /// <summary>
-        /// この関数は .NET 7.0 以降の場合 <c>short.Sign</c> を呼ぶ.
-        /// .NET 6.0 以前の場合, 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
-        /// 符号なし整数型の場合 <c>v</c> が 0 の場合は 0 を, 0 より大きい場合は 1 を返す.
-        /// </summary>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int Sign(short v) => short.Sign(v);
-
-        /// <summary>
-        /// この関数は .NET 7.0 以降の場合 <c>ushort.Sign</c> を呼ぶ.
-        /// .NET 6.0 以前の場合, 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
-        /// 符号なし整数型の場合 <c>v</c> が 0 の場合は 0 を, 0 より大きい場合は 1 を返す.
-        /// </summary>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int Sign(ushort v) => ushort.Sign(v);
-
-        /// <summary>
-        /// この関数は .NET 7.0 以降の場合 <c>byte.Sign</c> を呼ぶ.
-        /// .NET 6.0 以前の場合, 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
-        /// 符号なし整数型の場合 <c>v</c> が 0 の場合は 0 を, 0 より大きい場合は 1 を返す.
-        /// </summary>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int Sign(byte v) => byte.Sign(v);
-
-        /// <summary>
-        /// この関数は .NET 7.0 以降の場合 <c>sbyte.Sign</c> を呼ぶ.
-        /// .NET 6.0 以前の場合, 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
-        /// 符号なし整数型の場合 <c>v</c> が 0 の場合は 0 を, 0 より大きい場合は 1 を返す.
-        /// </summary>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int Sign(sbyte v) => sbyte.Sign(v);
-
-        /// <summary>
-        /// この関数は .NET 7.0 以降の場合 <c>float.Sign</c> を呼ぶ.
-        /// .NET 6.0 以前の場合, 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
-        /// 符号なし整数型の場合 <c>v</c> が 0 の場合は 0 を, 0 より大きい場合は 1 を返す.
-        /// </summary>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int Sign(float v) => float.Sign(v);
-
-        /// <summary>
-        /// この関数は .NET 7.0 以降の場合 <c>double.Sign</c> を呼ぶ.
-        /// .NET 6.0 以前の場合, 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
-        /// 符号なし整数型の場合 <c>v</c> が 0 の場合は 0 を, 0 より大きい場合は 1 を返す.
-        /// </summary>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int Sign(double v) => double.Sign(v);
-
-        /// <summary>
-        /// この関数は .NET 7.0 以降の場合 <c>decimal.Sign</c> を呼ぶ.
-        /// .NET 6.0 以前の場合, 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
-        /// 符号なし整数型の場合 <c>v</c> が 0 の場合は 0 を, 0 より大きい場合は 1 を返す.
-        /// </summary>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int Sign(decimal v) => decimal.Sign(v);
-
-#else
-
-        /// <summary>
-        /// この関数は .NET 7.0 以降の場合 <c>int.Sign</c> を呼ぶ.
-        /// .NET 6.0 以前の場合, 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
+        /// 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
         /// 符号なし整数型の場合 <c>v</c> が 0 の場合は 0 を, 0 より大きい場合は 1 を返す.
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int Sign(int v) => System.Math.Sign(v);
 
         /// <summary>
-        /// この関数は .NET 7.0 以降の場合 <c>long.Sign</c> を呼ぶ.
-        /// .NET 6.0 以前の場合, 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
+        /// 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
         /// 符号なし整数型の場合 <c>v</c> が 0 の場合は 0 を, 0 より大きい場合は 1 を返す.
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int Sign(long v) => System.Math.Sign(v);
 
         /// <summary>
-        /// この関数は .NET 7.0 以降の場合 <c>short.Sign</c> を呼ぶ.
-        /// .NET 6.0 以前の場合, 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
+        /// 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
         /// 符号なし整数型の場合 <c>v</c> が 0 の場合は 0 を, 0 より大きい場合は 1 を返す.
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int Sign(short v) => System.Math.Sign(v);
 
         /// <summary>
-        /// この関数は .NET 7.0 以降の場合 <c>sbyte.Sign</c> を呼ぶ.
-        /// .NET 6.0 以前の場合, 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
+        /// 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
         /// 符号なし整数型の場合 <c>v</c> が 0 の場合は 0 を, 0 より大きい場合は 1 を返す.
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int Sign(sbyte v) => System.Math.Sign(v);
 
         /// <summary>
-        /// この関数は .NET 7.0 以降の場合 <c>float.Sign</c> を呼ぶ.
-        /// .NET 6.0 以前の場合, 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
+        /// 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
         /// 符号なし整数型の場合 <c>v</c> が 0 の場合は 0 を, 0 より大きい場合は 1 を返す.
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int Sign(float v) => System.Math.Sign(v);
 
         /// <summary>
-        /// この関数は .NET 7.0 以降の場合 <c>double.Sign</c> を呼ぶ.
-        /// .NET 6.0 以前の場合, 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
+        /// 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
         /// 符号なし整数型の場合 <c>v</c> が 0 の場合は 0 を, 0 より大きい場合は 1 を返す.
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int Sign(double v) => System.Math.Sign(v);
 
         /// <summary>
-        /// この関数は .NET 7.0 以降の場合 <c>decimal.Sign</c> を呼ぶ.
-        /// .NET 6.0 以前の場合, 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
+        /// 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
         /// 符号なし整数型の場合 <c>v</c> が 0 の場合は 0 を, 0 より大きい場合は 1 を返す.
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int Sign(decimal v) => System.Math.Sign(v);
 
         /// <summary>
-        /// この関数は .NET 7.0 以降の場合 <c>uint.Sign</c> を呼ぶ.
-        /// .NET 6.0 以前の場合, 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
+        /// 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
         /// 符号なし整数型の場合 <c>v</c> が 0 の場合は 0 を, 0 より大きい場合は 1 を返す.
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int Sign(uint v) => (v == 0) ? 0 : 1;
 
         /// <summary>
-        /// この関数は .NET 7.0 以降の場合 <c>ulong.Sign</c> を呼ぶ.
-        /// .NET 6.0 以前の場合, 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
+        /// 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
         /// 符号なし整数型の場合 <c>v</c> が 0 の場合は 0 を, 0 より大きい場合は 1 を返す.
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int Sign(ulong v) => (v == 0) ? 0 : 1;
 
         /// <summary>
-        /// この関数は .NET 7.0 以降の場合 <c>ushort.Sign</c> を呼ぶ.
-        /// .NET 6.0 以前の場合, 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
+        /// 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
         /// 符号なし整数型の場合 <c>v</c> が 0 の場合は 0 を, 0 より大きい場合は 1 を返す.
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int Sign(ushort v) => (v == 0) ? 0 : 1;
 
         /// <summary>
-        /// この関数は .NET 7.0 以降の場合 <c>byte.Sign</c> を呼ぶ.
-        /// .NET 6.0 以前の場合, 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
+        /// 符号付き数値型の場合 <c>System.Math.Sign</c> を呼ぶ.
         /// 符号なし整数型の場合 <c>v</c> が 0 の場合は 0 を, 0 より大きい場合は 1 を返す.
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int Sign(byte v) => (v == 0) ? 0 : 1;
-
-#endif
 
     }
 }
