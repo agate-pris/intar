c087b1827ba973ae3710c98f01acf42367ae36f9
agate-pris <agate-pris@users.noreply.github.com>
2024-06-15 19:09:05 +0900

    不要な警告の抑制を削除

--- .generator/templates/Mathematics_math.cs
+++ .generator/templates/Mathematics_math.cs
@@ -13,23 +13,10 @@ using static Unity.Mathematics.math;
 #endif
 
 namespace AgatePris.Intar.Mathematics {
-
-#pragma warning disable IDE1006 // 命名スタイル
-
-#pragma warning disable IDE0079 // 不要な抑制を削除します
-#pragma warning disable CS8981 // 型名には、小文字の ASCII 文字のみが含まれています。このような名前は、プログラミング言語用に予約されている可能性があります。
-
     public static partial class math {
-
-#pragma warning restore CS8981 // 型名には、小文字の ASCII 文字のみが含まれています。このような名前は、プログラミング言語用に予約されている可能性があります。
-#pragma warning restore IDE0079 // 不要な抑制を削除します
-
         {%- for d in range(start=2, end=5) %}
             {{- self::aslong(i="int" ~ d, d=d) }}
         {%- endfor %}
 
     }
-
-#pragma warning restore IDE1006 // 命名スタイル
-
 }
