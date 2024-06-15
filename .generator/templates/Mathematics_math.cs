5154ccf9954b9e2d5c5b98530177194b9515891f
agate-pris <agate-pris@users.noreply.github.com>
2024-06-16 01:22:03 +0900

    aslong 削除

--- .generator/templates/Mathematics_math.cs
+++ .generator/templates/Mathematics_math.cs
@@ -1,14 +1,5 @@
 {% import "macros.cs" as macros %}
 
-{%- macro aslong(i, d) %}
-
-        public static long{{ d }} aslong(this {{ i }} x) => long{{ d }}(
-            x.x,
-            x.y{% if d > 2 %},
-            x.z{% if d > 3 %},
-            x.w{% endif %}{% endif %});
-{%- endmacro -%}
-
 {%- macro asbits(s, i, f, d) %}
 
         public static {{ macros::bits_type(s=s, i=i, f=f) }}{{ d }} asbits(this {{ macros::fixed_type(s=s, i=i, f=f) }}_{{ d }} x) => {{ macros::bits_type(s=s, i=i, f=f) }}{{ d }}(
@@ -25,10 +16,6 @@ using static Unity.Mathematics.math;
 
 namespace AgatePris.Intar.Mathematics {
     public static partial class math {
-        {%- for d in range(start=2, end=5) %}
-            {{- self::aslong(i="int" ~ d, d=d) }}
-        {%- endfor %}
-
         {%- for d in range(start=2, end=5) %}
             {%- for s in [true, false] %}
                 {%- for i in range(start=2, end=32) %}
