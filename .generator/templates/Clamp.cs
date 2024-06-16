148e35ec200930db843afcd6f3dc854619cfb0f9
agate-pris <agate-pris@users.noreply.github.com>
2024-06-16 23:33:16 +0900

    メソッド Clamp が属するクラスを変更し、浮動小数点数のオーバーロードを削除

--- .generator/templates/Clamp.cs
+++ .generator/templates/Clamp.cs
@@ -1,18 +1,19 @@
 {% macro clamp(type) %}
 
         /// <summary>
-        /// この関数は <c>Unity.Mathematics.math.clamp</c> や
-        /// <c>Intar.Mathematics.math.clamp</c> と異なり,
+        /// この関数は <c>Unity.Mathematics.math.clamp</c> と異なり,
         /// <c>min</c> が <c>max</c> より大きい場合, 例外を送出する.
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static {{ type }} Clamp({{ type }} v, {{ type }} min, {{ type }} max) {
+#if NET6_0_OR_GREATER
+            return System.Math.Clamp(v, min, max);
+#else
             if (min > max) {
                 throw new ArgumentException($"'{min}' cannot be greater than {max}.");
             }
-            return v < min
-                ? min : v > max
-                ? max : v;
+            return System.Math.Min(System.Math.Max(v, min), max);
+#endif
         }
 {%- endmacro -%}
 
@@ -20,8 +21,8 @@ using System;
 using System.Runtime.CompilerServices;
 
 namespace AgatePris.Intar {
-    public static partial class Math {
-{%- for type in ["int", "uint", "long", "ulong", "short", "ushort", "byte", "sbyte", "float", "double", "decimal"] %}
+    public static partial class Mathi {
+{%- for type in ["int", "uint", "long", "ulong", "short", "ushort", "byte", "sbyte"] %}
         {{- self::clamp(type = type) }}
 {%- endfor %}
 
