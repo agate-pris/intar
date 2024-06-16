4706a0a411dba884a7f34319c75b0f52bdc3eef1
agate-pris <agate-pris@users.noreply.github.com>
2024-06-16 19:28:42 +0900

    math.gen.cs 余計な using を削除

--- .generator/templates/Mathematics_math.cs
+++ .generator/templates/Mathematics_math.cs
@@ -1,10 +1,5 @@
 {% import "macros.cs" as macros %}
 
-#if UNITY_2018_3_OR_NEWER
-using Unity.Mathematics;
-using static Unity.Mathematics.math;
-#endif
-
 namespace AgatePris.Intar.Mathematics {
     public static partial class math {
     }
