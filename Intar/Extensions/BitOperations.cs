25173b9ac3981549a5c36ceccc3b536d6dec8c64
agate-pris <agate-pris@users.noreply.github.com>
2024-06-19 12:17:24 +0900

    BitOperationsExtension

---
+++ Intar/Extensions/BitOperations.cs
@@ -0,0 +1,15 @@
+using System.Runtime.CompilerServices;
+using static System.Runtime.CompilerServices.MethodImplOptions;
+
+namespace AgatePris.Intar.Extensions {
+    public static class BitOperations {
+        [MethodImpl(AggressiveInlining)] public static int LeadingZeroCount(this uint x) => Intar.BitOperations.LeadingZeroCount(x);
+        [MethodImpl(AggressiveInlining)] public static int LeadingZeroCount(this ulong x) => Intar.BitOperations.LeadingZeroCount(x);
+        [MethodImpl(AggressiveInlining)] public static int PopCount(this uint x) => Intar.BitOperations.PopCount(x);
+        [MethodImpl(AggressiveInlining)] public static int PopCount(this ulong x) => Intar.BitOperations.PopCount(x);
+        [MethodImpl(AggressiveInlining)] public static uint RotateLeft(this uint x, int k) => Intar.BitOperations.RotateLeft(x, k);
+        [MethodImpl(AggressiveInlining)] public static ulong RotateLeft(this ulong x, int k) => Intar.BitOperations.RotateLeft(x, k);
+        [MethodImpl(AggressiveInlining)] public static uint RotateRight(uint x, int k) => Intar.BitOperations.RotateRight(x, k);
+        [MethodImpl(AggressiveInlining)] public static ulong RotateRight(ulong x, int k) => Intar.BitOperations.RotateRight(x, k);
+    }
+}
