34ef499d49a297bec6e90a313cb3d0fded5a8411
agate-pris <agate-pris@users.noreply.github.com>
2024-07-09 01:38:50 +0900

    FixedTest using 追加

--- AgatePris.Intar.Tests/FixedTest.cs
+++ AgatePris.Intar.Tests/FixedTest.cs
@@ -1,11 +1,12 @@
+using AgatePris.Intar.Numerics;
 using NUnit.Framework;
 
 namespace AgatePris.Intar.Tests {
     public class FixedTest {
         [Test]
         public static void Test() {
-            var a = Numerics.I17F15.FromNum(1);
-            var b = Numerics.I17F15.FromNum(2);
+            var a = I17F15.FromNum(1);
+            var b = I17F15.FromNum(2);
             a += b;
             Assert.AreEqual(3, (int)a);
         }
