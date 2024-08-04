2cff2c3ba962230345a787c0ac10dd84d1ae8e8e
agate-pris <agate-pris@users.noreply.github.com>
2024-08-05 00:03:30 +0900

    BitOperations.cs 削除

--- AgatePris.Intar/BitOperations.cs
+++
@@ -1,8 +0,0 @@
-namespace AgatePris.Intar {
-    public static partial class BitOperations {
-#if !NET7_0_OR_GREATER
-        const int bitsOfInt = sizeof(int) * 8;
-        const int bitsOfLong = sizeof(long) * 8;
-#endif
-    }
-}
