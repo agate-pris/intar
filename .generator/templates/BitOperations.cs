b07f4090926d0927cc13d0adbc722bb0d77c4413
agate-pris <agate-pris@users.noreply.github.com>
2024-08-05 00:48:38 +0900

    BitOperations.cs RotateLeft RotateRight 追加

--- .generator/templates/BitOperations.cs
+++ .generator/templates/BitOperations.cs
@@ -1,9 +1,26 @@
+{% import "macros.cs" as macros %}
+
+using System.Runtime.CompilerServices;
+
 namespace AgatePris.Intar {
     public static partial class BitOperations {
 
-#if !NET7_0_OR_GREATER
+#if NET7_0_OR_GREATER
+{% for method in ['RotateLeft', 'RotateRight'] %}
+{%- for type in ['uint', 'ulong'] %}
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static {{ type }} {{ method }}({{ type }} x, int k) => System.Numerics.BitOperations.{{ method }}(x, k);
+{%- endfor %}
+{%- endfor %}
+
+#else
 {% for type in ['int', 'long'] %}
         const int bitsOf{{ type | capitalize }} = sizeof({{ type }}) * 8;
+{%- endfor %}
+{% for type in ['uint', 'ulong'] %}
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static {{ type }} RotateLeft({{ type }} x, int k) => (x << k) | (x >> (bitsOf{{ macros::signed_type(t=type) | capitalize }} - k));
+{%- endfor %}
+{%- for type in ['uint', 'ulong'] %}
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static {{ type }} RotateRight({{ type }} x, int k) => (x >> k) | (x << (bitsOf{{ macros::signed_type(t=type) | capitalize }} - k));
 {%- endfor %}
 
 #endif
