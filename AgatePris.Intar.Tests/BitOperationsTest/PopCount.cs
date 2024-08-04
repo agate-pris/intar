05a55e3eeb6452c5b2dbf9f829ca9f97966c2764
agate-pris <agate-pris@users.noreply.github.com>
2024-08-05 04:44:14 +0900

    PopCount テスト リファクタリング

--- AgatePris.Intar.Tests/BitOperationsTest/PopCount.cs
+++ AgatePris.Intar.Tests/BitOperationsTest/PopCount.cs
@@ -28,41 +28,51 @@ namespace AgatePris.Intar.Tests {
             return count;
         }
 
-        static void TestPopCountTestUint(uint x) {
-            var expected = PopCount(x);
-            var actual = BitOperations.PopCount(x);
-            if (expected != actual) {
-                Assert.Fail();
-            }
-        }
-
         [Test]
-        public static void PopCountTestUint() {
+        public static void TestPopCountUint() {
             Assert.AreEqual(0, BitOperations.PopCount(0U));
+            Assert.AreEqual(1, BitOperations.PopCount(1U));
+            Assert.AreEqual(1, BitOperations.PopCount(2U));
+            Assert.AreEqual(2, BitOperations.PopCount(3U));
+            Assert.AreEqual(1, BitOperations.PopCount(4U));
+            Assert.AreEqual(2, BitOperations.PopCount(5U));
+            Assert.AreEqual(2, BitOperations.PopCount(6U));
+            Assert.AreEqual(3, BitOperations.PopCount(7U));
+            Assert.AreEqual(1, BitOperations.PopCount(8U));
+            Assert.AreEqual(2, BitOperations.PopCount(9U));
             Assert.AreEqual(32, BitOperations.PopCount(uint.MaxValue));
 
             var processorCount = Environment.ProcessorCount;
+            var denominator = (ulong)processorCount;
             _ = Parallel.For(0, processorCount, n => {
-                var begin = uint.MaxValue * (ulong)n / (ulong)processorCount;
-                var end = uint.MaxValue * (ulong)(n + 1) / (ulong)processorCount;
-                for (var x = (uint)begin; x < (uint)end; ++x) {
-                    TestPopCountTestUint(x);
+                const uint offset = 10U;
+                const uint k = uint.MaxValue - offset;
+                var curr = (ulong)n;
+                var next = curr + 1;
+                var begin = offset + (uint)(k * curr / denominator);
+                var end = offset + (uint)(k * next / denominator);
+                for (var x = begin; x < end; ++x) {
+                    var expected = PopCount(x);
+                    var actual = BitOperations.PopCount(x);
+                    if (expected != actual) {
+                        Assert.Fail();
+                    }
                 }
             });
         }
 
-        static void TestPopCountTestUlong(ulong x) {
-            var expected = PopCount(x);
-            var actual = BitOperations.PopCount(x);
-            if (expected != actual) {
-                Assert.Fail();
-            }
-        }
-
         [Test]
-        public static void PopCountTestUlong() {
-
+        public static void TestPopCountUlong() {
             Assert.AreEqual(0, BitOperations.PopCount(0UL));
+            Assert.AreEqual(1, BitOperations.PopCount(1UL));
+            Assert.AreEqual(1, BitOperations.PopCount(2UL));
+            Assert.AreEqual(2, BitOperations.PopCount(3UL));
+            Assert.AreEqual(1, BitOperations.PopCount(4UL));
+            Assert.AreEqual(2, BitOperations.PopCount(5UL));
+            Assert.AreEqual(2, BitOperations.PopCount(6UL));
+            Assert.AreEqual(3, BitOperations.PopCount(7UL));
+            Assert.AreEqual(1, BitOperations.PopCount(8UL));
+            Assert.AreEqual(2, BitOperations.PopCount(9UL));
             Assert.AreEqual(64, BitOperations.PopCount(ulong.MaxValue));
 
             var processorCount = Environment.ProcessorCount;
@@ -72,7 +82,12 @@ namespace AgatePris.Intar.Tests {
                     rng.Jump();
                 }
                 for (var i = 0; i < 99999; ++i) {
-                    TestPopCountTestUlong(rng.Next());
+                    var x = rng.Next();
+                    var expected = PopCount(x);
+                    var actual = BitOperations.PopCount(x);
+                    if (expected != actual) {
+                        Assert.Fail();
+                    }
                 }
             });
         }
