3e24c80a8d12f5aec6ba4203f210312c33fa2a8f
agate-pris <agate-pris@users.noreply.github.com>
2024-08-04 19:01:19 +0900

    Mathi.gen.cs 更新

--- AgatePris.Intar/Mathi.gen.cs
+++ AgatePris.Intar/Mathi.gen.cs
@@ -132,5 +132,10 @@ namespace AgatePris.Intar {
 #endif
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static int Half(int x) => x / 2;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static uint Half(uint x) => x / 2;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static long Half(long x) => x / 2;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static ulong Half(ulong x) => x / 2;
+
     }
 }
