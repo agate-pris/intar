6cf5cd14c7d05463ac7ce80ba415eafc1c443057
agate-pris <agate-pris@users.noreply.github.com>
2024-08-05 00:49:49 +0900

    BitOperations.gen.cs 更新

--- AgatePris.Intar/BitOperations.gen.cs
+++ AgatePris.Intar/BitOperations.gen.cs
@@ -1,10 +1,24 @@
+using System.Runtime.CompilerServices;
+
 namespace AgatePris.Intar {
     public static partial class BitOperations {
 
-#if !NET7_0_OR_GREATER
+#if NET7_0_OR_GREATER
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static uint RotateLeft(uint x, int k) => System.Numerics.BitOperations.RotateLeft(x, k);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static ulong RotateLeft(ulong x, int k) => System.Numerics.BitOperations.RotateLeft(x, k);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static uint RotateRight(uint x, int k) => System.Numerics.BitOperations.RotateRight(x, k);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static ulong RotateRight(ulong x, int k) => System.Numerics.BitOperations.RotateRight(x, k);
+
+#else
 
         const int bitsOfInt = sizeof(int) * 8;
         const int bitsOfLong = sizeof(long) * 8;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static uint RotateLeft(uint x, int k) => (x << k) | (x >> (bitsOfInt - k));
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static ulong RotateLeft(ulong x, int k) => (x << k) | (x >> (bitsOfLong - k));
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static uint RotateRight(uint x, int k) => (x >> k) | (x << (bitsOfInt - k));
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static ulong RotateRight(ulong x, int k) => (x >> k) | (x << (bitsOfLong - k));
 
 #endif
 
