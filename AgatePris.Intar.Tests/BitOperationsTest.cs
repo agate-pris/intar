0ce747765b4042706f628b1c2ae4d364ee49c14c
agate-pris <agate-pris@users.noreply.github.com>
2024-08-05 08:37:42 +0900

    PopCount テスト 修正

--- AgatePris.Intar.Tests/BitOperationsTest.cs
+++ AgatePris.Intar.Tests/BitOperationsTest.cs
@@ -31,21 +31,23 @@ namespace AgatePris.Intar.Tests {
         [Test]
         public static void TestPopCountUint() {
             Assert.AreEqual(0, BitOperations.PopCount(0U));
-            Assert.AreEqual(1, BitOperations.PopCount(1U));
-            Assert.AreEqual(1, BitOperations.PopCount(2U));
-            Assert.AreEqual(2, BitOperations.PopCount(3U));
-            Assert.AreEqual(1, BitOperations.PopCount(4U));
-            Assert.AreEqual(2, BitOperations.PopCount(5U));
-            Assert.AreEqual(2, BitOperations.PopCount(6U));
-            Assert.AreEqual(3, BitOperations.PopCount(7U));
-            Assert.AreEqual(1, BitOperations.PopCount(8U));
-            Assert.AreEqual(2, BitOperations.PopCount(9U));
             Assert.AreEqual(32, BitOperations.PopCount(uint.MaxValue));
+            for (var i = 0; i < 32; ++i) {
+                var x = 1U << i;
+                Assert.AreEqual(1, BitOperations.PopCount(x));
+                Assert.AreEqual(31, BitOperations.PopCount(~x));
+
+                for (var j = i + 1; j < 32; ++j) {
+                    var y = 1U << j;
+                    Assert.AreEqual(2, BitOperations.PopCount(x | y));
+                    Assert.AreEqual(30, BitOperations.PopCount(~(x | y)));
+                }
+            }
 
             var processorCount = Environment.ProcessorCount;
             var denominator = (ulong)processorCount;
             _ = Parallel.For(0, processorCount, n => {
-                const uint offset = 10U;
+                const uint offset = 2U;
                 const uint k = uint.MaxValue - offset;
                 var curr = (ulong)n;
                 var next = curr + 1;
@@ -64,16 +66,18 @@ namespace AgatePris.Intar.Tests {
         [Test]
         public static void TestPopCountUlong() {
             Assert.AreEqual(0, BitOperations.PopCount(0UL));
-            Assert.AreEqual(1, BitOperations.PopCount(1UL));
-            Assert.AreEqual(1, BitOperations.PopCount(2UL));
-            Assert.AreEqual(2, BitOperations.PopCount(3UL));
-            Assert.AreEqual(1, BitOperations.PopCount(4UL));
-            Assert.AreEqual(2, BitOperations.PopCount(5UL));
-            Assert.AreEqual(2, BitOperations.PopCount(6UL));
-            Assert.AreEqual(3, BitOperations.PopCount(7UL));
-            Assert.AreEqual(1, BitOperations.PopCount(8UL));
-            Assert.AreEqual(2, BitOperations.PopCount(9UL));
             Assert.AreEqual(64, BitOperations.PopCount(ulong.MaxValue));
+            for (var i = 0; i < 64; ++i) {
+                var x = 1UL << i;
+                Assert.AreEqual(1, BitOperations.PopCount(x));
+                Assert.AreEqual(63, BitOperations.PopCount(~x));
+
+                for (var j = i + 1; j < 64; ++j) {
+                    var y = 1UL << j;
+                    Assert.AreEqual(2, BitOperations.PopCount(x | y));
+                    Assert.AreEqual(62, BitOperations.PopCount(~(x | y)));
+                }
+            }
 
             var processorCount = Environment.ProcessorCount;
             _ = Parallel.For(0, processorCount, n => {
