0a038f73b5141bd6f366a042db446f951bceebe7
agate-pris <agate-pris@users.noreply.github.com>
2024-08-05 22:48:23 +0900

    TestRotateLeft と TestRotateRight を移動

--- AgatePris.Intar.Tests/BitOperationsTest.cs
+++ AgatePris.Intar.Tests/BitOperationsTest.cs
@@ -155,5 +155,71 @@ namespace AgatePris.Intar.Tests {
                 }
             });
         }
+
+        [Test]
+        public static void TestRotateLeft() {
+            for (var i = 0; i < 32; ++i) {
+                Assert.AreEqual(0U, BitOperations.RotateLeft(0U, i));
+            }
+            for (var i = 0; i < 64; ++i) {
+                Assert.AreEqual(0UL, BitOperations.RotateLeft(0UL, i));
+            }
+            {
+                var k = new uint[] {
+                    0x0000_FFFFU, 0x00FF_FF00U, 0xFFFF_0000U, 0xFF00_00FFU,
+                };
+                for (var i = 0; i < 4; ++i) {
+                    Assert.AreEqual(k[i], BitOperations.RotateLeft(k[i], 32));
+                    Assert.AreEqual(k[(i + 1) % 4], BitOperations.RotateLeft(k[i], 8));
+                    Assert.AreEqual(k[(i + 2) % 4], BitOperations.RotateLeft(k[i], 16));
+                    Assert.AreEqual(k[(i + 3) % 4], BitOperations.RotateLeft(k[i], 24));
+                }
+            }
+            {
+                var k = new ulong[] {
+                    0x0000_0000_0000_FFFFUL, 0x0000_0000_FFFF_0000UL,
+                    0x0000_FFFF_0000_0000UL, 0xFFFF_0000_0000_0000UL,
+                };
+                for (var i = 0; i < 4; ++i) {
+                    Assert.AreEqual(k[i], BitOperations.RotateLeft(k[i], 64));
+                    Assert.AreEqual(k[(i + 1) % 4], BitOperations.RotateLeft(k[i], 16));
+                    Assert.AreEqual(k[(i + 2) % 4], BitOperations.RotateLeft(k[i], 32));
+                    Assert.AreEqual(k[(i + 3) % 4], BitOperations.RotateLeft(k[i], 48));
+                }
+            }
+        }
+
+        [Test]
+        public static void TestRotateRight() {
+            for (var i = 0; i < 32; ++i) {
+                Assert.AreEqual(0U, BitOperations.RotateRight(0U, i));
+            }
+            for (var i = 0; i < 64; ++i) {
+                Assert.AreEqual(0UL, BitOperations.RotateRight(0UL, i));
+            }
+            {
+                var k = new uint[] {
+                    0xFFFF_0000U, 0x00FF_FF00U, 0x0000_FFFFU, 0xFF00_00FFU,
+                };
+                for (var i = 0; i < 4; ++i) {
+                    Assert.AreEqual(k[i], BitOperations.RotateRight(k[i], 32));
+                    Assert.AreEqual(k[(i + 1) % 4], BitOperations.RotateRight(k[i], 8));
+                    Assert.AreEqual(k[(i + 2) % 4], BitOperations.RotateRight(k[i], 16));
+                    Assert.AreEqual(k[(i + 3) % 4], BitOperations.RotateRight(k[i], 24));
+                }
+            }
+            {
+                var k = new ulong[] {
+                    0xFFFF_0000_0000_0000UL, 0x0000_FFFF_0000_0000UL,
+                    0x0000_0000_FFFF_0000UL, 0x0000_0000_0000_FFFFUL,
+                };
+                for (var i = 0; i < 4; ++i) {
+                    Assert.AreEqual(k[i], BitOperations.RotateRight(k[i], 64));
+                    Assert.AreEqual(k[(i + 1) % 4], BitOperations.RotateRight(k[i], 16));
+                    Assert.AreEqual(k[(i + 2) % 4], BitOperations.RotateRight(k[i], 32));
+                    Assert.AreEqual(k[(i + 3) % 4], BitOperations.RotateRight(k[i], 48));
+                }
+            }
+        }
     }
 }
