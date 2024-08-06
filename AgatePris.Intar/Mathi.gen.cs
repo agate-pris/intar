8ae2d5681c25e53789cfdf537ffec6caabfafb5b
agate-pris <agate-pris@users.noreply.github.com>
2024-08-06 12:31:25 +0900

    Mathi.gen.cs 更新

--- AgatePris.Intar/Mathi.gen.cs
+++ AgatePris.Intar/Mathi.gen.cs
@@ -3,6 +3,45 @@ using System.Runtime.CompilerServices;
 
 namespace AgatePris.Intar {
     public static partial class Mathi {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static int AsinInternal(int x) {
+            var ret = 391 * x;
+
+            const int k1 = 50762241;
+            ret = ((k1 - ret) >> 15) * x;
+
+            const int k2 = 144994038;
+            ret = ((k2 - ret) >> 15) * x;
+
+            const int k3 = 1073695665;
+            ret = (k3 - ret) >> 15;
+
+            const int k0 = 1 << 15;
+
+            return (int)Sqrt(k0 * (k0 - (uint)x)) * ret;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static uint Acos(int x) {
+            var negate = x < 0;
+            var sign = Math.Sign(x);
+            x = AsinInternal(negate ? -x : x);
+            x = sign * (x - (1 << 30));
+            if (x < 0) {
+                return (uint)(x + (1 << 30));
+            } else {
+                return (uint)x + (1 << 30);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int Asin(int x) {
+            var negate = x < 0;
+            var sign = Math.Sign(x);
+            x = AsinInternal(negate ? -x : x);
+            return sign * ((1 << 30) - x);
+        }
+
         internal static class Atan {
             internal const int One = 1 << 15;
             internal const int OneNeg = -One;
