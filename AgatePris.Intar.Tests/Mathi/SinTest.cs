683992e5924690962d035b214550620b63212daa
agate-pris <agate-pris@users.noreply.github.com>
2024-09-14 15:44:19 +0900

    modified:   AgatePris.Intar.Tests/Mathi/SinTest.cs

--- AgatePris.Intar.Tests/Mathi/SinTest.cs
+++ AgatePris.Intar.Tests/Mathi/SinTest.cs
@@ -31,7 +31,6 @@ namespace AgatePris.Intar.Tests.Mathi {
             new SinCase(Intar.Mathi.SinP2, Intar.Mathi.CosP2, "sin_p2.json", 0.056010),
             new SinCase(Intar.Mathi.SinP3A16384, Intar.Mathi.CosP3A16384, "sin_p3.json", 0.020017),
             new SinCase(Intar.Mathi.SinP4A7373, Intar.Mathi.CosP4A7373, "sin_p4_7373.json", 0.001114),
-            new SinCase(Intar.Mathi.SinP4A7385, Intar.Mathi.CosP4A7385, "sin_p4_7385.json", 0.001180),
             new SinCase(Intar.Mathi.SinP5A51436, Intar.Mathi.CosP5A51436, "sin_p5_51436.json", 0.000223),
         };
 
