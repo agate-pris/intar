edacb0c4c045cc00c0c0323772c57bfb1e7119ef
agate-pris <agate-pris@users.noreply.github.com>
2024-10-04 12:48:10 +0900

    Mathi.cs 更新

--- .generator/templates/Mathi.cs
+++ .generator/templates/Mathi.cs
@@ -90,30 +90,36 @@ namespace AgatePris.Intar {
     public static class Mathi {
         const decimal Pi = 3.1415926535897932384626433833m;
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal static int AsinInternal(int x) {
-            // (2^15) / (0.5 * PI) * 0.0187293
-            // = 390.707370478933
-            var ret = 391 * x;
+        internal static class AsinInternal {
+            const decimal K = 2 / Pi;
 
-            // (2^30) / (0.5 * PI) * 0.074261
-            // = 50762240.9295814
-            const int k1 = 50762241;
-            ret = ((k1 - ret) >> 15) * x;
+            internal const uint P3U32A = (uint)(0.5m + ((1U << 31) * K * (1U << 1) * 1.5707288m));
+            internal const uint P3U32B = (uint)(0.5m + ((1U << 31) * K * (1U << 3) * 0.2121144m));
+            internal const uint P3U32C = (uint)(0.5m + ((1U << 31) * K * (1U << 5) * 0.0742610m));
+            internal const uint P3U32D = (uint)(0.0m + ((1U << 31) * K * (1U << 7) * 0.0187293m));
 
-            // (2^30) / (0.5 * PI) * 0.2121144
-            // = 144994038.289729
-            const int k2 = 144994038;
-            ret = ((k2 - ret) >> 15) * x;
-
-            // (2^30) / (0.5 * PI) * 1.5707288
-            // = 1073695665.02784
-            const int k3 = 1073695665;
-            ret = (k3 - ret) >> 15;
-
-            const int k0 = 1 << 15;
-
-            return (int)Sqrt(k0 * (k0 - (uint)x)) * ret;
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            internal static uint P3(uint x) {
+                // (2^15) / (0.5 * PI) * 0.0187293
+                // = 390.707370478933
+                // (2^30) / (0.5 * PI) * 0.074261
+                // = 50762240.9295814
+                //const int k1 = 50762241;
+                // (2^30) / (0.5 * PI) * 0.2121144
+                // = 144994038.289729
+                //const int k2 = 144994038;
+                // (2^30) / (0.5 * PI) * 1.5707288
+                // = 1073695665.02784
+                //const int k3 = 1073695665;
+                //const int k0 = 1 << 15;
+                uint ret;
+                ret = (P3U32D + (1U << (15 + 7 - 5 - 1))) >> (15 + 7 - 5);
+                ret = (P3U32C - (ret * x)) >> (15 + 5 - 3);
+                ret = (P3U32B - (ret * x)) >> (15 + 3 - 1);
+                ret = (P3U32A - (ret * x)) >> (15 + 1);
+                const uint one = 1U << 15;
+                return Sqrt(one * (one - x)) * ret;
+            }
         }
 
         /// <summary>
@@ -126,7 +132,7 @@ namespace AgatePris.Intar {
         public static uint Acos(int x) {
             var negate = x < 0;
             var sign = Math.Sign(x);
-            x = AsinInternal(negate ? -x : x);
+            x = (int)AsinInternal.P3((uint)(negate ? -x : x));
             x = sign * (x - (1 << 30));
             if (x < 0) {
                 return (uint)(x + (1 << 30));
@@ -145,7 +151,7 @@ namespace AgatePris.Intar {
         public static int Asin(int x) {
             var negate = x < 0;
             var sign = Math.Sign(x);
-            x = AsinInternal(negate ? -x : x);
+            x = (int)AsinInternal.P3((uint)(negate ? -x : x));
             return sign * ((1 << 30) - x);
         }
 
