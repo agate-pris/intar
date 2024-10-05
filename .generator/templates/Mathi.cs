eb8e2947c5783d18782164f50460c22d389ae772
agate-pris <agate-pris@users.noreply.github.com>
2024-10-05 21:03:36 +0900

    Mathi.cs

--- .generator/templates/Mathi.cs
+++ .generator/templates/Mathi.cs
@@ -91,53 +91,62 @@ namespace AgatePris.Intar {
         const decimal Pi = 3.1415926535897932384626433833m;
 
         internal static class AsinInternal {
-            const decimal K = 2 / Pi;
+            const decimal Frac2Pi = 2 / Pi;
 
-            internal const uint P3U32A = (uint)(0.5m + ((1U << 31) * K * (1U << 1) * 1.5707288m));
-            internal const uint P3U32B = (uint)(0.5m + ((1U << 31) * K * (1U << 3) * 0.2121144m));
-            internal const uint P3U32C = (uint)(0.5m + ((1U << 31) * K * (1U << 5) * 0.0742610m));
-            internal const uint P3U32D = (uint)(0.0m + ((1U << 31) * K * (1U << 7) * 0.0187293m));
+            {%- set p1 = ['int',  32, '' ] %}
+            {%- set p2 = ['long', 64, 'L'] %}
+            {%- set ps = [p1, p2] %}
+            {%- for p in ps %}
+            const decimal Z{{ p[1] }} = Frac2Pi * (1U{{ p[2] }} << {{ p[1] - 1 }});
+            {%- endfor %}
+            {%- for p in ps %}
+            internal const u{{ p[0] }} P3U{{ p[1] }}A = (u{{ p[0] }})(0.5m + (Z{{ p[1] }} * (1 << 1) * 1.5707288m));
+            internal const u{{ p[0] }} P3U{{ p[1] }}B = (u{{ p[0] }})(0.5m + (Z{{ p[1] }} * (1 << 3) * 0.2121144m));
+            internal const u{{ p[0] }} P3U{{ p[1] }}C = (u{{ p[0] }})(0.5m + (Z{{ p[1] }} * (1 << 5) * 0.0742610m));
+            internal const u{{ p[0] }} P3U{{ p[1] }}D = (u{{ p[0] }})(0.0m + (Z{{ p[1] }} * (1 << 7) * 0.0187293m));
+            {%- endfor %}
+
+            {%- for p in ps %}
 
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            internal static uint P3(uint x) {
-                // (2^15) / (0.5 * PI) * 0.0187293
-                // = 390.707370478933
-                // (2^30) / (0.5 * PI) * 0.074261
-                // = 50762240.9295814
-                //const int k1 = 50762241;
-                // (2^30) / (0.5 * PI) * 0.2121144
-                // = 144994038.289729
-                //const int k2 = 144994038;
-                // (2^30) / (0.5 * PI) * 1.5707288
-                // = 1073695665.02784
-                //const int k3 = 1073695665;
-                //const int k0 = 1 << 15;
-                uint ret;
-                ret = (P3U32D + (1U << (15 + 7 - 5 - 1))) >> (15 + 7 - 5);
-                ret = (P3U32C - (ret * x)) >> (15 + 5 - 3);
-                ret = (P3U32B - (ret * x)) >> (15 + 3 - 1);
-                ret = (P3U32A - (ret * x)) >> (15 + 1);
-                const uint one = 1U << 15;
-                return Sqrt(one * (one - x)) * ret;
+            internal static u{{ p[0] }} P3(u{{ p[0] }} x) {
+                var y = 1U{{ p[2] }} << ({{ p[1] / 2 - 1 }} + 7 - 5);
+                y = (P3U{{ p[1] }}D + (y / 2)) >> ({{ p[1] / 2 - 1 }} + 7 - 5);
+                y = (P3U{{ p[1] }}C - (y * x)) >> ({{ p[1] / 2 - 1 }} + 5 - 3);
+                y = (P3U{{ p[1] }}B - (y * x)) >> ({{ p[1] / 2 - 1 }} + 3 - 1);
+                y = (P3U{{ p[1] }}A - (y * x)) >> ({{ p[1] / 2 - 1 }} + 1 + 1);
+                const u{{ p[0] }} one = 1U{{ p[2] }} << {{ p[1] / 2 - 1 }};
+                return Sqrt(one * (one - x)) * y;
             }
+            {%- endfor %}
         }
+
+        {%- set p1=['int',  32, '',  3] %}
+        {%- set p2=['long', 64, 'L', 3] %}
+        {%- for p in [p1, p2] %}
 
         /// <summary>
         /// 逆余弦を近似する｡
         /// </summary>
         /// <param name="x">2 の 15 乗を 1 とする余弦</param>
-        /// <returns>0 以上 π 以下の､ π を 2 の 30 乗で表した角度｡</returns>
-        {{- self::asin_equation() }}
+        /// <returns>0 以上 π 以下の､ π を 2 の 31 乗で表した角度｡</returns>
+        /// <remarks>
+        /// <div class="CAUTION alert alert-info">
+        /// <h5>Caution</h5>
+        /// <para>このメソッドは引数 <c>x</c> が範囲外 (-32768 未満または 32768 より大きい値) の場合､ 誤った値を返します｡</para>
+        /// </div>
+        /// <div class="WARNING alert alert-info">
+        /// <h5>Warning</h5>
+        /// <para>このメソッドは引数 <c>x</c> が範囲外 (-32768 未満または 32768 より大きい値) の場合､ 例外を送出する場合があります｡</para>
+        /// </div>
+        /// </remarks>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static uint Acos(int x) {
-            var negate = x < 0;
-            var sign = Math.Sign(x);
-            x = (int)AsinInternal.P3((uint)(negate ? -x : x));
-            x = sign * (x - (1 << 30));
-            if (x < 0) {
-                return (uint)(x + (1 << 30));
-            } else {
-                return (uint)x + (1 << 30);
+        public static u{{ p[0] }} AcosP{{ p[3] }}({{ p[0] }} x) {
+            const u{{ p[0] }} pi = 1U{{ p[2] }} << {{ p[1] - 1 }};
+            switch (Math.Sign(x)) {
+                case 0: return pi / 2;
+                case 1: return AsinInternal.P3((u{{ p[0] }})x);
+                default: return pi - AsinInternal.P3((u{{ p[0] }})-x);
             }
         }
 
@@ -145,15 +154,27 @@ namespace AgatePris.Intar {
         /// 逆正弦を近似する｡
         /// </summary>
         /// <param name="x">2 の 15 乗を 1 とする正弦</param>
-        /// <returns>-π/2 以上 π/2 以下の､ π を 2 の 30 乗で表した角度｡</returns>
-        {{- self::asin_equation() }}
+        /// <returns>0 以上 π 以下の､ π を 2 の 31 乗で表した角度｡</returns>
+        /// <remarks>
+        /// <div class="CAUTION alert alert-info">
+        /// <h5>Caution</h5>
+        /// <para>このメソッドは引数 <c>x</c> が範囲外 (-32768 未満または 32768 より大きい値) の場合､ 誤った値を返します｡</para>
+        /// </div>
+        /// <div class="WARNING alert alert-info">
+        /// <h5>Warning</h5>
+        /// <para>このメソッドは引数 <c>x</c> が範囲外 (-32768 未満または 32768 より大きい値) の場合､ 例外を送出する場合があります｡</para>
+        /// </div>
+        /// </remarks>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int Asin(int x) {
-            var negate = x < 0;
-            var sign = Math.Sign(x);
-            x = (int)AsinInternal.P3((uint)(negate ? -x : x));
-            return sign * ((1 << 30) - x);
+        public static {{ p[0] }} AsinP{{ p[3] }}({{ p[0] }} x) {
+            const {{ p[0] }} fracPi2 = 1{{ p[2] }} << {{ p[1] - 2 }};
+            switch (Math.Sign(x)) {
+                case 0: return 0;
+                case 1: return fracPi2 - ({{ p[0] }})AsinInternal.P3((u{{ p[0] }})x);
+                default: return ({{ p[0] }})AsinInternal.P3((u{{ p[0] }})-x) - fracPi2;
+            }
         }
+        {%- endfor %}
 
         internal static class AtanInternal {
             // Round(K * Inv(a / K))
