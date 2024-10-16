f1d32f9c459c050059cb07d86f812bc16e9cd4cf
agate-pris <agate-pris@users.noreply.github.com>
2024-10-16 23:29:52 +0900

    Overflowing.gen.cs 更新

--- AgatePris.Intar/Overflowing.gen.cs
+++ AgatePris.Intar/Overflowing.gen.cs
@@ -315,9 +315,9 @@ namespace AgatePris.Intar {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool OverflowingAdd(uint x, uint y, out uint result) {
             // unchecked コンテキストでは、整数の演算結果はラップアラウンドする。
-            // 特に、符号なし整数のそれについては、C# 以外の言語 (C++ など) でも同様の振る舞いが期待できる。
-            // ラップアラウンドした場合、結果は両辺のどちらの値よりも必ず小さくなる。
             result = unchecked(x + y);
+
+            // 結果がより小さくなったらオーバーフロー。
             return result < x;
         }
 
@@ -367,9 +367,9 @@ namespace AgatePris.Intar {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool OverflowingAdd(ulong x, ulong y, out ulong result) {
             // unchecked コンテキストでは、整数の演算結果はラップアラウンドする。
-            // 特に、符号なし整数のそれについては、C# 以外の言語 (C++ など) でも同様の振る舞いが期待できる。
-            // ラップアラウンドした場合、結果は両辺のどちらの値よりも必ず小さくなる。
             result = unchecked(x + y);
+
+            // 結果がより小さくなったらオーバーフロー。
             return result < x;
         }
 
@@ -418,14 +418,12 @@ namespace AgatePris.Intar {
         /// </example>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool OverflowingAdd(int x, int y, out int result) {
-            // 両辺の符号が異なる場合、オーバーフローは発生しない。
-            // オーバーフローが発生すると、
-            // 両辺の符号が正の場合、結果は両辺のどちらの値よりも必ず小さくなる。
-            // 両辺の符号が負の場合、結果は両辺のどちらの値よりも必ず大きくなる。
+            // unchecked コンテキストでは、整数の演算結果はラップアラウンドする。
             result = unchecked(x + y);
-            return
-                ((x < 0) && (y < 0) && (result > x)) ||
-                ((x > 0) && (y > 0) && (result < x));
+
+            // 右辺が 0 未満の場合、結果がより大きくなったらオーバーフロー。
+            // それ以外の場合、結果がより小さくなったらオーバーフロー。
+            return y < 0 ? result > x : result < x;
         }
 
         /// <summary>
@@ -481,14 +479,12 @@ namespace AgatePris.Intar {
         /// </example>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool OverflowingAdd(long x, long y, out long result) {
-            // 両辺の符号が異なる場合、オーバーフローは発生しない。
-            // オーバーフローが発生すると、
-            // 両辺の符号が正の場合、結果は両辺のどちらの値よりも必ず小さくなる。
-            // 両辺の符号が負の場合、結果は両辺のどちらの値よりも必ず大きくなる。
+            // unchecked コンテキストでは、整数の演算結果はラップアラウンドする。
             result = unchecked(x + y);
-            return
-                ((x < 0) && (y < 0) && (result > x)) ||
-                ((x > 0) && (y > 0) && (result < x));
+
+            // 右辺が 0 未満の場合、結果がより大きくなったらオーバーフロー。
+            // それ以外の場合、結果がより小さくなったらオーバーフロー。
+            return y < 0 ? result > x : result < x;
         }
 
         /// <summary>
