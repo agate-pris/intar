2588cdfba51394f9703c69c526e3bad82133c2b1
agate-pris <agate-pris@users.noreply.github.com>
2024-12-10 00:00:48 +0900

    modified:   Intar1991.Tests/MatrixTest.cs

--- Intar1991.Tests/MatrixTest.cs
+++ Intar1991.Tests/MatrixTest.cs
@@ -33,25 +33,41 @@ namespace Intar1991.Tests {
             return d;
         }
 
+#if UNITY_5_3_OR_NEWER
+        static double Delta(UnityEngine.Matrix4x4 e, Matrix3x3I2F30 a) {
+            var d = 0.0;
+            d = Math.Max(d, Math.Abs(e.m00 - a.C0.X.LossyToSingle()));
+            d = Math.Max(d, Math.Abs(e.m01 - a.C1.X.LossyToSingle()));
+            d = Math.Max(d, Math.Abs(e.m02 - a.C2.X.LossyToSingle()));
+            d = Math.Max(d, Math.Abs(e.m03 - 0.0));
+            d = Math.Max(d, Math.Abs(e.m10 - a.C0.Y.LossyToSingle()));
+            d = Math.Max(d, Math.Abs(e.m11 - a.C1.Y.LossyToSingle()));
+            d = Math.Max(d, Math.Abs(e.m12 - a.C2.Y.LossyToSingle()));
+            d = Math.Max(d, Math.Abs(e.m13 - 0.0));
+            d = Math.Max(d, Math.Abs(e.m20 - a.C0.Z.LossyToSingle()));
+            d = Math.Max(d, Math.Abs(e.m21 - a.C1.Z.LossyToSingle()));
+            d = Math.Max(d, Math.Abs(e.m22 - a.C2.Z.LossyToSingle()));
+            d = Math.Max(d, Math.Abs(e.m23 - 0.0));
+            d = Math.Max(d, Math.Abs(e.m30 - 0.0));
+            d = Math.Max(d, Math.Abs(e.m31 - 0.0));
+            d = Math.Max(d, Math.Abs(e.m32 - 0.0));
+            d = Math.Max(d, Math.Abs(e.m33 - 1.0));
+            return d;
+        }
+#endif
+
 #if UNITY_2018_1_OR_NEWER
-        static double Delta(Unity.Mathematics.float4x4 e, Matrix3x3I2F30 a) {
+        static double Delta(Unity.Mathematics.float3x3 e, Matrix3x3I2F30 a) {
             var d = 0.0;
             d = Math.Max(d, Math.Abs(e.c0.x - a.C0.X.LossyToSingle()));
             d = Math.Max(d, Math.Abs(e.c1.x - a.C1.X.LossyToSingle()));
             d = Math.Max(d, Math.Abs(e.c2.x - a.C2.X.LossyToSingle()));
-            d = Math.Max(d, Math.Abs(e.c3.x - 0.0));
             d = Math.Max(d, Math.Abs(e.c0.y - a.C0.Y.LossyToSingle()));
             d = Math.Max(d, Math.Abs(e.c1.y - a.C1.Y.LossyToSingle()));
             d = Math.Max(d, Math.Abs(e.c2.y - a.C2.Y.LossyToSingle()));
-            d = Math.Max(d, Math.Abs(e.c3.y - 0.0));
             d = Math.Max(d, Math.Abs(e.c0.z - a.C0.Z.LossyToSingle()));
             d = Math.Max(d, Math.Abs(e.c1.z - a.C1.Z.LossyToSingle()));
             d = Math.Max(d, Math.Abs(e.c2.z - a.C2.Z.LossyToSingle()));
-            d = Math.Max(d, Math.Abs(e.c3.z - 0.0));
-            d = Math.Max(d, Math.Abs(e.c0.w - 0.0));
-            d = Math.Max(d, Math.Abs(e.c1.w - 0.0));
-            d = Math.Max(d, Math.Abs(e.c2.w - 0.0));
-            d = Math.Max(d, Math.Abs(e.c3.w - 1.0));
             return d;
         }
 #endif
@@ -62,11 +78,9 @@ namespace Intar1991.Tests {
             var rng = new Xoroshiro128StarStar(1, 2);
             var axis = QuaternionTest.RandomAxis(ref rng);
             var angle = QuaternionTest.RandomI17F15(ref rng);
-
             var dm1 = 0.0;
             var dm2 = 0.0;
             var dm3 = 0.0;
-
             for (var i = 0; i < 32768; i++) {
                 var a = Matrix3x3I2F30.AxisAngleP5(axis, angle);
 
@@ -100,9 +114,10 @@ namespace Intar1991.Tests {
 
 #if UNITY_2018_1_OR_NEWER
                 {
-                    var e = Unity.Mathematics.float4x4.AxisAngle(
+                    var e = Unity.Mathematics.float3x3.AxisAngle(
                         (Unity.Mathematics.float3)axis,
-                        (float)(angle.LossyToSingle() * Math.PI / 2));
+                        (float)(angle.LossyToSingle() * Math.PI / 2)
+                    );
                     var d = Delta(e, a);
                     if (d > delta) {
                         QuaternionTest.LogError($"d:{d} axis:{axis} angle:{angle} e:{e} a:{a}");
@@ -125,22 +140,56 @@ namespace Intar1991.Tests {
             const double delta = 0.0000003;
             var rng = new Xoroshiro128StarStar(1, 2);
             var dm1 = 0.0;
+            var dm2 = 0.0;
+            var dm3 = 0.0;
             for (var i = 0; i < 32786; i++) {
                 var q = QuaternionTest.RandomQuaternion(ref rng);
                 var a = (Matrix3x3I2F30)q;
 
                 {
-                    var qf = (System.Numerics.Quaternion)q;
-                    var e = System.Numerics.Matrix4x4.CreateFromQuaternion(qf);
+                    var e = System.Numerics.Matrix4x4.CreateFromQuaternion(
+                        (System.Numerics.Quaternion)q
+                    );
                     var d = Delta(e, a);
                     if (d > delta) {
                         QuaternionTest.Log($"d:{d} q:{q} e:{e} a:{a}");
                     }
                     dm1 = Math.Max(dm1, d);
                 }
+
+#if UNITY_5_3_OR_NEWER
+                {
+                    var e = UnityEngine.Matrix4x4.Rotate(
+                        (UnityEngine.Quaternion)q
+                    );
+                    var d = Delta(e, a);
+                    if (d > delta) {
+                        QuaternionTest.Log($"d:{d} q:{q} e:{e} a:{a}");
+                    }
+                    dm2 = Math.Max(dm2, d);
+                }
+#endif // UNITY_5_3_OR_NEWER
+
+#if UNITY_2018_1_OR_NEWER
+                {
+                    var e = new Unity.Mathematics.float3x3(
+                        (Unity.Mathematics.quaternion)q
+                    );
+                    var d = Delta(e, a);
+                    if (d > delta) {
+                        QuaternionTest.Log($"d:{d} q:{q} e:{e} a:{a}");
+                    }
+                    dm3 = Math.Max(dm3, d);
+                }
+#endif // UNITY_2018_1_OR_NEWER
+
             }
             Assert.Less(dm1, delta);
+            Assert.Less(dm2, delta);
+            Assert.Less(dm3, delta);
             QuaternionTest.Log($"dm1:{dm1}");
+            QuaternionTest.Log($"dm2:{dm2}");
+            QuaternionTest.Log($"dm3:{dm3}");
         }
     }
 } // namespace Intar1991.Tests
