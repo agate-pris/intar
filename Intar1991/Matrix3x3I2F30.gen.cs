6e4aae8f21115b1506ef561e6d5c156ed31efe6a
agate-pris <agate-pris@users.noreply.github.com>
2024-12-11 21:45:24 +0900

    modified:   ../Intar1991/Matrix3x3I2F30.gen.cs

--- Intar1991/Matrix3x3I2F30.gen.cs
+++ Intar1991/Matrix3x3I2F30.gen.cs
@@ -27,11 +27,15 @@ namespace Intar1991 {
         #endregion
         #region Constructors
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public Matrix3x3I2F30(Vector3Int32 c0Repr, Vector3Int32 c1Repr, Vector3Int32 c2Repr) {
+        Matrix3x3I2F30(Vector3Int32 c0Repr, Vector3Int32 c1Repr, Vector3Int32 c2Repr) {
             C0Repr = c0Repr;
             C1Repr = c1Repr;
             C2Repr = c2Repr;
         }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Matrix3x3I2F30(Vector3I2F30 c0, Vector3I2F30 c1, Vector3I2F30 c2)
+            : this(c0.Repr, c1.Repr, c2.Repr) { }
         #endregion
         #region Identity
         public static readonly Matrix3x3I2F30 Identity = new Matrix3x3I2F30(
@@ -45,7 +49,7 @@ namespace Intar1991 {
             new Vector3Int32(0, 0, 0),
             new Vector3Int32(0, 0, 0),
             new Vector3Int32(0, 0, 0)
-        )
+        );
         #endregion
         #region Properties
         public Vector3I2F30 C0 {
@@ -128,6 +132,26 @@ namespace Intar1991 {
             );
         }
         #endregion
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Matrix3x3I2F30 operator *(Matrix3x3I2F30 left, Matrix3x3I2F30 right) {
+            return new Matrix3x3I2F30(
+                new Vector3I2F30(
+                    I2F30.UncheckedLossyFrom(left.R0.UncheckedDot(right.C0)),
+                    I2F30.UncheckedLossyFrom(left.R1.UncheckedDot(right.C0)),
+                    I2F30.UncheckedLossyFrom(left.R2.UncheckedDot(right.C0))
+                ),
+                new Vector3I2F30(
+                    I2F30.UncheckedLossyFrom(left.R0.UncheckedDot(right.C1)),
+                    I2F30.UncheckedLossyFrom(left.R1.UncheckedDot(right.C1)),
+                    I2F30.UncheckedLossyFrom(left.R2.UncheckedDot(right.C1))
+                ),
+                new Vector3I2F30(
+                    I2F30.UncheckedLossyFrom(left.R0.UncheckedDot(right.C2)),
+                    I2F30.UncheckedLossyFrom(left.R1.UncheckedDot(right.C2)),
+                    I2F30.UncheckedLossyFrom(left.R2.UncheckedDot(right.C2))
+                )
+            );
+        }
         #region Conversion
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
