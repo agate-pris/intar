c42590d6d38fef25dc83db220cb60abea74d5a18
agate-pris <agate-pris@users.noreply.github.com>
2024-12-19 09:20:52 +0900

    AfffineTransformTest 追加

---
+++ Intar1991.Tests/AffineTransformTest.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace Intar1991.Tests {
+    public class AffineTransformTest {
+        [Test]
+        public static void TestTrs() {
+        }
+    }
+}
