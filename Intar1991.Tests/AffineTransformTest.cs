864ca2c5d5236bc460a842529640809694667337
agate-pris <agate-pris@users.noreply.github.com>
2024-12-19 09:37:44 +0900

    modified:   Intar1991.Tests/AffineTransformTest.cs

--- Intar1991.Tests/AffineTransformTest.cs
+++ Intar1991.Tests/AffineTransformTest.cs
@@ -7,6 +7,9 @@ namespace Intar1991.Tests {
     public class AffineTransformTest {
         [Test]
         public static void TestTrs() {
+            var rng = new Xoroshiro128StarStar(1, 2);
+            for (var i = 0; i < 32768; i++) {
+            }
         }
     }
 }
