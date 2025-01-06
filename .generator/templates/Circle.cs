6ad6b02206cabfb1e09bbcc3bc4b790522a9cfc0
agate-pris <agate-pris@users.noreply.github.com>
2025-01-06 23:11:55 +0900

    円判定を表現する構造体 Geometry.Circle2I17F15 を追加 (#283)
    * Circle.cs 追加
    * settings.json Circle.cs 登録
    * Circle2I17F15 追加

---
+++ .generator/templates/Circle.cs
@@ -0,0 +1,82 @@
+{%- import "macros.cs" as macros %}
+{%- set dim = 2 %}
+{%- set components = ['X', 'Y', 'Z', 'W']|slice(end=dim) %}
+{%- set component   = 'I17F15' %}
+{%- set component_u = 'U17F15' %}
+{%- set type = 'Circle' ~ dim ~ component -%}
+using System;
+using System.Runtime.CompilerServices;
+
+namespace {{ namespace }}.Geometry {
+    [Serializable]
+    public struct {{ type }} : IEquatable<{{ type }}>, IFormattable {
+        public {{ macros::vector_type(dim=2, type=component) }} Center;
+        public {{ component_u }} Radius;
+        #region Construction
+        public {{ type }}({{ macros::vector_type(dim=2, type=component) }} center, {{ component_u }} radius) {
+            Center = center;
+            Radius = radius;
+        }
+        #endregion
+        #region IEquatable
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Equals({{ type }} other) {
+            return Center.Equals(other.Center) && Radius.Equals(other.Radius);
+        }
+        #endregion
+        #region Object
+        public override bool Equals(object obj) {
+            return obj is {{ type }} o && Equals(o);
+        }
+
+        public override int GetHashCode() => HashCode.Combine(
+            Center, Radius
+        );
+
+        public override string ToString() {
+            return $"{{ '{{' }}Center:{Center} Radius:{Radius}{{ '}}' }}";
+        }
+        #endregion
+        #region IFormattable
+        public string ToString(string format, IFormatProvider formatProvider) {
+            return $"{{ '{{' }}Center:{Center.ToString(format, formatProvider)} Radius:{Radius.ToString(format, formatProvider)}{{ '}}' }}";
+        }
+        #endregion
+        #region IMultiplyOperators
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static {{ type }} operator *(AffineTransform3{{ component }} left, {{ type }} right) {
+            {%- for c in components %}
+            var s{{ c }} = left.DecomposeScale{{ c }}();
+            {%- endfor %}
+            var center = new Vector3{{ component }}(
+                {%- for i in range(end=3) %}
+                {%- if i < dim -%}
+                right.Center.{{ components[i] }}
+                {%- else -%}
+                {{ component }}.Zero
+                {%- endif %}
+                {%- if not loop.last -%}, {% endif %}
+                {%- endfor %});
+            return new {{ type }}(
+                (left * center).
+                {%- for i in range(end=dim) -%}
+                {{ components[i] }}
+                {%- endfor %}(),
+                right.Radius * sX
+                {%- for i in range(start=1, end=dim) -%}
+                .Max(s{{ components[i] }})
+                {%- endfor %}
+            );
+        }
+        #endregion
+        #region Disjoint, Intersects
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Disjoint({{ type }} other) {
+            var r = Radius + other.Radius;
+            return Center.DistanceSquared(other.Center) > r.BigMul(r);
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Intersects({{ type }} other) => !Disjoint(other);
+        #endregion
+    }
+}
---
+++ Intar/Geometry/Circle2I17F15.gen.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Intar.Geometry {
+    [Serializable]
+    public struct Circle2I17F15 : IEquatable<Circle2I17F15>, IFormattable {
+        public Vector2I17F15 Center;
+        public U17F15 Radius;
+        #region Construction
+        public Circle2I17F15(Vector2I17F15 center, U17F15 radius) {
+            Center = center;
+            Radius = radius;
+        }
+        #endregion
+        #region IEquatable
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Equals(Circle2I17F15 other) {
+            return Center.Equals(other.Center) && Radius.Equals(other.Radius);
+        }
+        #endregion
+        #region Object
+        public override bool Equals(object obj) {
+            return obj is Circle2I17F15 o && Equals(o);
+        }
+
+        public override int GetHashCode() => HashCode.Combine(
+            Center, Radius
+        );
+
+        public override string ToString() {
+            return $"{{Center:{Center} Radius:{Radius}}}";
+        }
+        #endregion
+        #region IFormattable
+        public string ToString(string format, IFormatProvider formatProvider) {
+            return $"{{Center:{Center.ToString(format, formatProvider)} Radius:{Radius.ToString(format, formatProvider)}}}";
+        }
+        #endregion
+        #region IMultiplyOperators
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Circle2I17F15 operator *(AffineTransform3I17F15 left, Circle2I17F15 right) {
+            var sX = left.DecomposeScaleX();
+            var sY = left.DecomposeScaleY();
+            var center = new Vector3I17F15(right.Center.X, right.Center.Y, I17F15.Zero);
+            return new Circle2I17F15(
+                (left * center).XY(),
+                right.Radius * sX.Max(sY)
+            );
+        }
+        #endregion
+        #region Disjoint, Intersects
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Disjoint(Circle2I17F15 other) {
+            var r = Radius + other.Radius;
+            return Center.DistanceSquared(other.Center) > r.BigMul(r);
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Intersects(Circle2I17F15 other) => !Disjoint(other);
+        #endregion
+    }
+}
