d116310e01f34c51c54404ed536ea55e99f42b8c
agate-pris <agate-pris@users.noreply.github.com>
2025-01-11 02:04:58 +0900

    固定小数点数 インスペクタの値が 10 進数で切りの良い値になるように修正

--- Intar.Editor/U2F62Drawer.gen.cs
+++ Intar.Editor/U2F62Drawer.gen.cs
@@ -11,6 +11,55 @@ namespace Intar.Editor {
             var max = (float)U2F62.MaxValue;
             return Mathf.Clamp(value, min, max);
         }
+        internal static float Restore(ulong bits) {
+            if (bits == 0) {
+                return 0;
+            }
+            if (bits == ulong.MaxValue) {
+                return (ulong.MaxValue + 1.0f) / U2F62.OneRepr;
+            }
+
+            var isNegative = bits < 0;
+            var lowerBound = bits;
+            var upperBound = bits + 1;
+            var result = System.Math.Abs((float)(bits / U2F62.OneRepr));
+            var precision = 0.1f;
+
+            for (var digit = 0; digit < 10; digit++) {
+                // 上限を以下の最も大きい値を選ぶ.
+                // その値が下限以上であれば, それが正解.
+                for (var num = 0; num < 10; num++) {
+                    var current = result + (precision * num);
+                    var next = result + (precision * (num + 1));
+
+                    // 次の値が上限以上の場合,
+                    // 現在の値を確定し次の桁へ進む.
+                    // upperBound が浮動小数点数に変換された時に精度を失った場合,
+                    // 0 方向への丸めが行われることを想定している.
+                    // その場合, upperBound「以上」という条件は厳密に成立する.
+                    if (next * U2F62.OneRepr >= upperBound) {
+                        result = current;
+                        break;
+                    }
+
+                    // 現在の値が下限以上の場合, それを返す.
+                    // current は next 以下であるため,
+                    // current * U2F62.OneRepr は upperBound 以下である.
+                    // よって current * U2F62.OneRepr は整数型に安全にキャストできる.
+                    // キャストした値は 0 方向に丸められるため,
+                    // lowerBound「以上」という条件は厳密に成立する.
+                    if ((uint)(current * U2F62.OneRepr) >= lowerBound) {
+                        return isNegative ? -current : current;
+                    }
+                }
+                precision *= 0.1f;
+            }
+
+            // 桁数が足りなかった場合のためのフォールバック処理.
+            // 固定小数点数の内部表現に戻す時,
+            // キャストで 0 方向に丸められた時に値が小さくならないように 0.5 を足す.
+            return (bits + (Mathf.Sign(bits) * 0.5f)) / U2F62.OneRepr;
+        }
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
             // Using BeginProperty / EndProperty on the parent property means that
             // prefab override logic works on the entire property.
@@ -20,7 +69,7 @@ namespace Intar.Editor {
 
             // すでに値がキャッシュされている場合はそれを使う
             // それ以外の場合, プロパティから値を取得してキャッシュする
-            var value = cache ?? (float)bits.ulongValue / U2F62.OneRepr;
+            var value = cache ?? Restore(bits.ulongValue);
 
             // UI を表示 & 入力を取得
             EditorGUI.BeginChangeCheck();
