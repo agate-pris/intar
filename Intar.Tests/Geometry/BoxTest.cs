7d877301fe36ec579de7ed04fc13056a504aa3ed
agate-pris <agate-pris@users.noreply.github.com>
2025-05-28 09:20:28 +0900

    modified:   BoxTest.cs

--- Intar.Tests/Geometry/BoxTest.cs
+++ Intar.Tests/Geometry/BoxTest.cs
@@ -1,7 +1,12 @@
+using Intar.Geometry;
+using Intar.Rand;
+using NUnit.Framework;
 using System.Collections.Generic;
 
-using Intar.Geometry;
-using NUnit.Framework;
+#if !UNITY_5_3_OR_NEWER && NET462
+using System.Drawing;
+using System.Drawing.Imaging;
+#endif
 
 namespace Intar.Tests.Geometry {
     public class BoxTest {
@@ -386,6 +391,182 @@ namespace Intar.Tests.Geometry {
                 Assert.IsFalse(b3.Intersects(b1));
                 Assert.IsFalse(b1.Overlaps(b3));
                 Assert.IsFalse(b3.Overlaps(b1));
+            }
+        }
+
+#if !UNITY_5_3_OR_NEWER && NET462
+        static Point Transform(Vector2I17F15 p) {
+            var x = (int)(32 * (float)p.X);
+            var y = (int)(32 * (float)p.Y);
+            return new Point(x + 128, y + 128);
+        }
+#endif
+
+        [Test]
+        public static void TestBounds() {
+            var rng = new Xoroshiro128StarStar(1, 2);
+            for (var z = 0; z < 65536; z++) {
+                var a = I17F15.FromBits(rng.Next(-65536, 65536));
+                var b = I17F15.FromBits(rng.Next(-65536, 65536));
+                var c = I17F15.FromBits(rng.Next(-65536, 65536));
+                var d = I17F15.FromBits(rng.Next(-65536, 65536));
+                var e = I17F15.FromBits(rng.Next(-65536, 65536));
+                var f = I17F15.FromBits(rng.Next(-65536, 65536));
+                var g = I17F15.FromBits(rng.Next(-65536, 65536));
+                var h = I17F15.FromBits(rng.Next(-65536, 65536));
+                var i = I17F15.FromBits(rng.Next(-65536, 65536));
+                var j = I17F15.FromBits(rng.Next(-65536, 65536));
+                var k = I17F15.FromBits(rng.Next(-65536, 65536));
+                var l = I17F15.FromBits(rng.Next(-65536, 65536));
+
+                var bounds = new Aabb2I17F15(new Segment2I17F15(
+                    new Vector2I17F15(a, b),
+                    new Vector2I17F15(c, d)
+                ));
+                var box = new Box2I17F15(
+                    new AffineTransform2I17F15(
+                        new Matrix2x2I17F15(
+                            new Vector2I17F15(g, h),
+                            new Vector2I17F15(i, j)
+                        ),
+                        new Vector2I17F15(k, l)
+                    ),
+                    new Vector2I17F15(e, f)
+                );
+                {
+                    var p0 = box.P1;
+                    var p1 = box.P2;
+                    var p2 = box.P3;
+                    var p3 = box.P4;
+                    var p4 = new Vector2I17F15(bounds.MaxX, bounds.MinY);
+                    var p5 = new Vector2I17F15(bounds.MinX, bounds.MinY);
+                    var p6 = new Vector2I17F15(bounds.MinX, bounds.MaxY);
+                    var p7 = new Vector2I17F15(bounds.MaxX, bounds.MaxY);
+                    bool expected1, expected2;
+                    {
+                        var e0 = new Segment2I17F15(p0, p1);
+                        var e1 = new Segment2I17F15(p1, p2);
+                        var e2 = new Segment2I17F15(p2, p3);
+                        var e3 = new Segment2I17F15(p3, p0);
+                        var e4 = new Segment2I17F15(p4, p5);
+                        var e5 = new Segment2I17F15(p5, p6);
+                        var e6 = new Segment2I17F15(p6, p7);
+                        var e7 = new Segment2I17F15(p7, p4);
+                        expected1 =
+                            bounds.Intersects(p0) ||
+                            bounds.Intersects(p1) ||
+                            bounds.Intersects(p2) ||
+                            bounds.Intersects(p3) ||
+                            box.Intersects(p4) ||
+                            box.Intersects(p5) ||
+                            box.Intersects(p6) ||
+                            box.Intersects(p7) ||
+                            e0.Intersects(e4) ||
+                            e0.Intersects(e5) ||
+                            e0.Intersects(e6) ||
+                            e0.Intersects(e7) ||
+                            e1.Intersects(e4) ||
+                            e1.Intersects(e5) ||
+                            e1.Intersects(e6) ||
+                            e1.Intersects(e7) ||
+                            e2.Intersects(e4) ||
+                            e2.Intersects(e5) ||
+                            e2.Intersects(e6) ||
+                            e2.Intersects(e7) ||
+                            e3.Intersects(e4) ||
+                            e3.Intersects(e5) ||
+                            e3.Intersects(e6) ||
+                            e3.Intersects(e7);
+                        expected2 =
+                            bounds.Overlaps(p0) ||
+                            bounds.Overlaps(p1) ||
+                            bounds.Overlaps(p2) ||
+                            bounds.Overlaps(p3) ||
+                            box.Overlaps(p4) ||
+                            box.Overlaps(p5) ||
+                            box.Overlaps(p6) ||
+                            box.Overlaps(p7) ||
+                            e0.Overlaps(e4) ||
+                            e0.Overlaps(e5) ||
+                            e0.Overlaps(e6) ||
+                            e0.Overlaps(e7) ||
+                            e1.Overlaps(e4) ||
+                            e1.Overlaps(e5) ||
+                            e1.Overlaps(e6) ||
+                            e1.Overlaps(e7) ||
+                            e2.Overlaps(e4) ||
+                            e2.Overlaps(e5) ||
+                            e2.Overlaps(e6) ||
+                            e2.Overlaps(e7) ||
+                            e3.Overlaps(e4) ||
+                            e3.Overlaps(e5) ||
+                            e3.Overlaps(e6) ||
+                            e3.Overlaps(e7);
+                    }
+                    var actual1 = box.Intersects(bounds);
+                    var actual2 = box.Overlaps(bounds);
+                    if (expected1 != actual1 || expected2 != actual2) {
+#if !UNITY_5_3_OR_NEWER && NET462
+                        var red = new Pen(Color.Red);
+                        var blue = new Pen(Color.Blue);
+                        var width = 256;
+                        var height = 256;
+                        var bitmap = new Bitmap(width, height);
+                        var graphics = Graphics.FromImage(bitmap);
+                        graphics.Clear(Color.White);
+                        var pt0 = Transform(p0);
+                        var pt1 = Transform(p1);
+                        var pt2 = Transform(p2);
+                        var pt3 = Transform(p3);
+                        var pt4 = Transform(p4);
+                        var pt5 = Transform(p5);
+                        var pt6 = Transform(p6);
+                        var pt7 = Transform(p7);
+                        graphics.DrawLine(red, pt0, pt1);
+                        graphics.DrawLine(red, pt1, pt2);
+                        graphics.DrawLine(red, pt2, pt3);
+                        graphics.DrawLine(red, pt3, pt0);
+                        graphics.DrawLine(blue, pt4, pt5);
+                        graphics.DrawLine(blue, pt5, pt6);
+                        graphics.DrawLine(blue, pt6, pt7);
+                        graphics.DrawLine(blue, pt7, pt4);
+                        bitmap.Save($"output{z}.png", ImageFormat.Png);
+#endif
+                        Assert.Fail();
+                    }
+                }
+            }
+            {
+                var box = new Box2I17F15(
+                    new AffineTransform2I17F15(
+                        Matrix2x2I17F15.Identity,
+                        Vector2I17F15.Zero
+                    ), Vector2I17F15.One
+                );
+                var b0 = new Aabb2I17F15(new Segment2I17F15(
+                    Vector2I17F15.Zero,
+                    Vector2I17F15.Zero
+                ));
+                var b1 = new Aabb2I17F15(new Segment2I17F15(
+                    Vector2I17F15.UnitX,
+                    Vector2I17F15.UnitX
+                ));
+                var b2 = new Aabb2I17F15(new Segment2I17F15(
+                    Vector2I17F15.UnitY,
+                    Vector2I17F15.UnitY
+                ));
+                var b3 = new Aabb2I17F15(new Segment2I17F15(
+                    Vector2I17F15.One,
+                    Vector2I17F15.One
+                ));
+                Assert.IsTrue(box.Intersects(b0));
+                Assert.IsTrue(box.Intersects(b1));
+                Assert.IsTrue(box.Intersects(b2));
+                Assert.IsTrue(box.Intersects(b3));
+                Assert.IsTrue(box.Overlaps(b0));
+                Assert.IsFalse(box.Overlaps(b1));
+                Assert.IsFalse(box.Overlaps(b2));
+                Assert.IsFalse(box.Overlaps(b3));
             }
         }
     }
