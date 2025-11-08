db0088dc660154a04f0e93fee7bfbb88cd4b0ff2
agate-pris <agate-pris@users.noreply.github.com>
2025-11-08 14:58:40 +0900

    KeyframeI17F15 コンストラクタを追加 (#348)

--- Intar.Tests/AnimationCurveTest.cs
+++ Intar.Tests/AnimationCurveTest.cs
@@ -8,16 +8,22 @@ namespace Intar.Tests {
             var b = I17F15.FromBits(2000);
             var c = I17F15.FromBits(3000);
             var d = I17F15.FromBits(4000);
+            var e = I17F15.FromBits(5000);
             var k = new KeyframeI17F15();
             Assert.IsTrue(I17F15.Zero == k.Time);
             Assert.IsTrue(I17F15.Zero == k.Value);
             Assert.IsTrue(I17F15.Zero == k.InTangent);
             Assert.IsTrue(I17F15.Zero == k.OutTangent);
-            k = new KeyframeI17F15(a, b, c, d);
+            k = new KeyframeI17F15(a, b);
             Assert.IsTrue(a == k.Time);
             Assert.IsTrue(b == k.Value);
-            Assert.IsTrue(c == k.InTangent);
-            Assert.IsTrue(d == k.OutTangent);
+            Assert.IsTrue(I17F15.Zero == k.InTangent);
+            Assert.IsTrue(I17F15.Zero == k.OutTangent);
+            k = new KeyframeI17F15(b, c, d, e);
+            Assert.IsTrue(b == k.Time);
+            Assert.IsTrue(c == k.Value);
+            Assert.IsTrue(d == k.InTangent);
+            Assert.IsTrue(e == k.OutTangent);
         }
     }
 }
